39ad2aa76b989b4ebed4eec1da2725a4231ac4d5
Seunggyu Lee <dltmdrb98@gmail.com>
2023-08-18 23:51:02 +0900

    Initial commit

---
+++ src/main/java/com/allinone/devtimeserver/DevtimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.allinone.devtimeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtimeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtimeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allinone/devtimeserver/DevtimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allinone.devtimeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtimeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
